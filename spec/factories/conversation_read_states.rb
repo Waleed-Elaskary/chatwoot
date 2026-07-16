d0f038f6b5790392d0599180898033dc97cbb1bb
Waleed M. Abdelkawy <Waleed.Elaskary@gmail.com>
2026-07-16 10:54:21 -0500

    Fix factory before(:build) dead hook and feature flag column overflow

--- spec/factories/conversation_read_states.rb
+++ spec/factories/conversation_read_states.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     conversation
     last_seen_at { Time.current }
 
-    before(:build) do |read_state|
+    after(:build) do |read_state|
       read_state.account ||= read_state.conversation&.account
       read_state.user ||= create(:user, account: read_state.account)
     end
